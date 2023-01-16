d8977e2aae2c0f5a1f7bad24ce263f2b5c41fd4f
deejay104 <matthieu@les-mnms.net>
2023-01-16 17:04:52 +0000

    Update core to resolve bug with dte_creat for baptemes

--- class/bapteme.inc.php
+++ class/bapteme.inc.php
@@ -45,7 +45,6 @@ class bapteme_class extends objet_core
 		"id_avion" => Array("type" => "number", "index" => "1", ),
 		"id_resa" => Array("type" => "number", "index"=>1),
 		"description" => Array("type" => "text" ),
-		"dte_creat" => Array("type" => "datetime","readonly"=>1 ),
 	);
 
 	
