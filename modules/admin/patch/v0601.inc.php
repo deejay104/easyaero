10cae61cef14b6f669d80b83820b28bf9b30c924
matthieu <matthieu@les-mnms.net>
2026-03-06 10:26:40 +0000

    bug with v0601 patch

--- modules/admin/patch/v0601.inc.php
+++ modules/admin/patch/v0601.inc.php
@@ -1,9 +1,11 @@
 <?php
-
+	// --------------------------
     $t=array("idvol","uid_pilote","uid_instructeur","uid_avion","status","type","dte_vol","module","nb_att","nb_rmg","tps_theo","sid_instructeur","sdte_instructeur","sip_instructeur","nonce_instructeur");
 
 	$query="SELECT * FROM ".$MyOpt["tbl"]."_synthese WHERE skey_instructeur<>'' ORDER BY id";
 	$res=$sql->Query($query);
+
+	$q=array();
 
 	for($i=0; $i<$sql->rows; $i++)
 	{ 
@@ -22,14 +24,22 @@
 		$hmachash=hash_hmac('sha256', $payload, $hmacKey);
 		$hash=hash('sha256', $hmachash);
 
-        $query="UPDATE `".$MyOpt["tbl"]."_synthese` SET nonce_instructeur='".$nonce."', sip_instructeur='null', skey_instructeur='".$hash."' WHERE id='".$sql->data["id"]."'";
-		$sql->Update($query);
+        $q[]="UPDATE `".$MyOpt["tbl"]."_synthese` SET nonce_instructeur='".$nonce."', sip_instructeur='null', skey_instructeur='".$hash."' WHERE id='".$sql->data["id"]."'";
+		//$sql->Update($query);
     }
 
+  	foreach($q as $i=>$query)
+	{
+		$sql->Update($query);
+	}
+
+	// --------------------------
     $t=array("idvol","uid_pilote","uid_instructeur","uid_avion","status","type","dte_vol","module","nb_att","nb_rmg","tps_theo","sid_pilote","sdte_pilote","sip_pilote","nonce_pilote");
 
     $query="SELECT * FROM ".$MyOpt["tbl"]."_synthese WHERE skey_pilote<>'' ORDER BY id";
 	$res=$sql->Query($query);
+
+	$q=array();
 
 	for($i=0; $i<$sql->rows; $i++)
 	{ 
@@ -48,8 +58,12 @@
 		$hmachash=hash_hmac('sha256', $payload, $hmacKey);
 		$hash=hash('sha256', $hmachash);
 
-        $query="UPDATE `".$MyOpt["tbl"]."_synthese` SET nonce_pilote='".$nonce."', sip_pilote='null', skey_pilote='".$hash."' WHERE id='".$sql->data["id"]."'";
-		$sql->Update($query);
+        $q[]="UPDATE `".$MyOpt["tbl"]."_synthese` SET nonce_pilote='".$nonce."', sip_pilote='null', skey_pilote='".$hash."' WHERE id='".$sql->data["id"]."'";
+		//$sql->Update($query);
     }
 
+	foreach($q as $i=>$query)
+	{
+		$sql->Update($query);
+	}
 ?>
